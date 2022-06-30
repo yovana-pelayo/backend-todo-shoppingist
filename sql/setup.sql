47e0743ec918df15019fe0a2649473f93fc9bf9b
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-06-29 22:45:32 -0700

    created a userService class that sets rules forour password and user nam and hashes the password. A little fuzz on that line 16 is doing. cIs it creating a user everything the user sr our new user. Thee password is hashed out via the number of salt rounds and stores the hashed password. N

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,11 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+
+DROP TABLE IF EXISTS usersSL;
+
+CREATE TABLE usersSL (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email VARCHAR NOT NULL UNIQUE,
+    passwordHash VARCHAR NOT NULL
+);
