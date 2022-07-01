05477ef65264e07990c436172199be3958fa52db
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-07-01 14:02:07 -0700

    created a test that checks if user is signed in and if a cookie was stored with user email and password for

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const mockUser = {
   email: 'blue@yahoo.com',
@@ -12,14 +13,23 @@ describe('user routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('POST creates new user', async () => {
+  it('POST /api/v1/users creates new user', async () => {
     const res = await request(app).post('/api/v1/users').send(mockUser);
     const { email } = mockUser;
-   
     expect(res.body).toEqual({
       id: expect.any(String),
       email,
     });
+  });
+  it('POST /sessions logs in new user and creates a cookie', async () => {
+    await UserService.create(mockUser);
+    const { email, password } = mockUser;
+    const res = await request(app)
+      .post('/api/v1/users/sessions')
+      .send({ email, password });
+    console.log(res.body);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({ message: 'Signed in successfully!' });
   });
   afterAll(() => {
     pool.end();
