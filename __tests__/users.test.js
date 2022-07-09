475c49b174e1398cd04c2d18659043669c45c1f4
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-07-09 14:08:35 -0700

    added secrets and updated sql table

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -6,7 +6,7 @@ const UserService = require('../lib/services/UserService');
 
 const mockUser = {
   email: 'blue@yahoo.com',
-  password: '123456'
+  password: '123456',
 };
 
 const registerAndLogin = async (userProps = {}) => {
@@ -21,7 +21,7 @@ const registerAndLogin = async (userProps = {}) => {
 };
 
 
-//  we are assigning an email to be user info, then we are storing that user information in a token and sending the email and password that authenticates the user. This info is stored in a token to be used by user but what is the difference between this and creating a user? Is it just the token with data storage? Like, now that the user is created we can call it and use it so.. I think I get it. 
+// agent allows token to be stored 
 describe('user routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -37,11 +37,13 @@ describe('user routes', () => {
 
 
   it('signs in an existing user', async () => {
-    await request(app).post('/api/v1/users').send(mockUser);
-    const res = await request(app)
-      .post('/api/v1/users/sessions')
-      .send({ email: 'kyle@example.com', password: '123456' });
-    expect(res.status).toEqual(200);
+    const [agent, user] = await registerAndLogin();
+    const me = await agent.get('/api/v1/users/me');
+    expect(me.body).toEqual({
+      ...user,
+      exp: expect.any(Number),
+      iat: expect.any(Number),
+    });
   });
   afterAll(() => {
     pool.end();
